60bf3e1a701e5c3a264e766016d00a6979631369
jinseok <ohjinseok2412@gmail.com>
2025-10-28 13:00:53 +0900

    20202941 오진석 제출

--- tests.py
+++ tests.py
@@ -78,14 +78,14 @@ matcher_ratio = features.RatioFeatureMatcher()
 # TODO7 - SSD matching
 # ------------------------------
 # Step 1. SSD matcher를 이용해 두 이미지의 MOPS 디스크립터 매칭을 수행하시오.
-matches_ssd = matcher_ssd.__________(__________, __________)
+matches_ssd = matcher_ssd.matchFeatures(desc_mops_1, desc_mops_2)
 
 # Step 2. 거리(distance)를 기준으로 정렬하여 상위 150개의 매칭만 선택하시오.
-matches_ssd = sorted(matches_ssd, key=lambda x: x.__________)[:150]
+matches_ssd = sorted(matches_ssd, key=lambda x: x.distance)[:150]
 
 # Step 3. 매칭 결과를 시각화하여 PNG로 저장하시오.
 ssd_vis = cv2.drawMatches(
-    img1, d1, img2, d2, ________, None,
+    img1, d1, img2, d2, matches_ssd, None,
     matchColor=(0,255,0), singlePointColor=(255,0,0),
     flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS
 )
@@ -96,14 +96,14 @@ print("✅ TODO7 (SSD) match result saved → results/TODO7_SSD_matches.png")
 # TODO8 - Ratio matching
 # ------------------------------
 # Step 1. Ratio matcher를 이용해 두 이미지의 MOPS 디스크립터 매칭을 수행하시오.
-matches_ratio = matcher_ratio.__________(__________, __________)
+matches_ratio = matcher_ratio.matchFeatures(desc_mops_1, desc_mops_2)
 
 # Step 2. distance를 기준으로 정렬하여 상위 150개의 매칭만 선택하시오.
-matches_ratio = sorted(matches_ratio, key=lambda x: x.__________)[:150]
+matches_ratio = sorted(matches_ratio, key=lambda x: x.distance)[:150]
 
 # Step 3. 매칭 결과를 시각화하여 PNG로 저장하시오.
 ratio_vis = cv2.drawMatches(
-    img1, d1, img2, d2, ________, None,
+    img1, d1, img2, d2, matches_ratio, None,
     matchColor=(0,255,0), singlePointColor=(255,0,0),
     flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS
 )
@@ -111,3 +111,14 @@ cv2.imwrite("results/TODO8_Ratio_matches.png", ratio_vis)
 print("✅ TODO8 (Ratio) match result saved → results/TODO8_Ratio_matches.png")
 
 print("🎯 All TODO1–8 visualizations done! Files saved in 'results/'")
+
+
+''' 
+- TODO 8단계의 매칭이 잘된 이유를 features.py코드를 바탕으로 구체적으로 작성
+
+- features.py 의 기존 SSD 방식은 단순히 가장 가까운 점 하나만 고르는 방식인 반면, 
+Ratio Matching(TODO8)은 가장 가까운 점과 두번째로 가까운 점의 거리 비율을 계산하여 신뢰도를 평가한다.
+이때 비율이 작은 경우에만 신뢰할 수 있는 매칭으로 판단한다.
+이는 특징점이 여러 후보와 유사할 경우 모호한 매칭을 걸러내므로 정확한 대응점만 남게 된다.
+
+'''
